581a41f2ea3dba0f54cbbcc4ee110a800449710d
unknown <rubiodelatorredidac@elpuig.xeill.net>
2026-09-26 14:52:52 +0200

    AgendaV1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Bienvenido a la agenda seleciona una opcion");
+            System.out.println("  1. Crear un contacto");
+            System.out.println("  2. Buscar contacto existente");
+            System.out.println("  3. Crear informacion de un contacto existente");
+            System.out.println("  4. Borrar un contacto ya existente");
+            System.out.println("  5. Salir de la agenda");
+
     }
 }
