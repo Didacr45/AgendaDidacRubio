05e1c9dc93372d25d4a313e0ea5b16c2cba83a83
unknown <rubiodelatorredidac@elpuig.xeill.net>
2026-09-26 15:13:50 +0200

    AgendaV1.1

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Bienvenido a la agenda seleciona una opcion");
             System.out.println("  1. Crear un contacto");
             System.out.println("  2. Buscar contacto existente");
@@ -11,5 +13,15 @@ public class Main {
             System.out.println("  4. Borrar un contacto ya existente");
             System.out.println("  5. Salir de la agenda");
 
+        int opcion1 = sc.nextInt();
+
+        switch (opcion1) {
+            case 1:
+                System.out.println(" Has escogido la opcion de Crear un contacto");
+                break;
+        
+            default:
+                break;
+        }
     }
 }
